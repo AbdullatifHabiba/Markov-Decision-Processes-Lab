5fc922f368467a238e8207693218d5230122aaab
MohamedElgreatly <mohamed.ahmed.elgreatly@gmail.com>
2022-12-14 19:59:30 +0200

    first commit

---
+++ src/HelpingMethods.java
@@ -0,0 +1,66 @@
+public class HelpingMethods {
+
+    public static char findDirection(int i,int j,double[][]gridArr) {
+        double maximumV = 0;
+        char maxChar = 'R';
+        if (i + 1 < gridArr.length) {
+            maximumV = gridArr[i + 1][j];
+            maxChar = 'D';
+        }
+        if (i - 1 >= 0 && maximumV<gridArr[i - 1][j]) {
+
+            maximumV = gridArr[i - 1][j];
+            maxChar = 'U';
+
+        }
+        if (j+1 <3 && maximumV<gridArr[i][j+1]) {
+
+            maximumV = gridArr[i][j+1];
+            maxChar = 'R';
+
+        } if (j-1 >=0 && maximumV<gridArr[i][j-1]) {
+
+            maximumV = gridArr[i][j-1];
+            maxChar = 'L';
+
+        }
+
+        return maxChar;
+    }
+
+    public static void edit_printPolicy(double[][]gridArr,char[][]grid_policy) {
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                grid_policy[i][j]=findDirection(i,j,gridArr);
+            }
+        }
+    }
+
+    public static double Q_value(int i, int j,char[][] grid_policy,double[][] gridArr,double discount,int[][] rewardArr){
+
+        double q=0;
+        if (grid_policy[i][j] == 'R') {
+            if (j < 2){q = .8 * (rewardArr[i][j + 1] + discount * gridArr[i][j + 1]);}
+            if (i > 0){ q += .1 * (rewardArr[i - 1][j] + discount * gridArr[i - 1][j]); }
+            if(i<2){q += .1 * (rewardArr[i + 1][j] + discount * gridArr[i + 1][j]);}
+        }
+        else if (grid_policy[i][j]=='U') {
+            if (i < 2){  q = .8 * (rewardArr[i + 1][j] + discount * gridArr[i + 1][j]);}
+            if (j > 0){q += .1 * (rewardArr[i][j - 1] + discount * gridArr[i][j - 1]);}
+            if (j < 2){q += .1 * (rewardArr[i][j + 1] + discount * gridArr[i][j + 1]);}
+        }
+        else if (grid_policy[i][j]=='D') {
+            if (i > 0){ q = .8 * (rewardArr[i - 1][j] + discount * gridArr[i - 1][j]) ;}
+            if (j > 0){q += .1 * (rewardArr[i][j - 1] + discount * gridArr[i][j - 1]);}
+            if (j < 2){ q += .1 * (rewardArr[i][j + 1] + discount * gridArr[i][j + 1]);}
+        }
+        else if (grid_policy[i][j]=='L') {
+            if (j > 0){q = .8 * (rewardArr[i][j - 1] + discount * gridArr[i][j - 1]) ;}
+            if (i > 0) {q += .1 * (rewardArr[i - 1][j] + discount * gridArr[i - 1][j]);}
+            if (i < 2){q += .1 * (rewardArr[i + 1][j] + discount * gridArr[i + 1][j]);}
+        }
+        return q;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static boolean compare2Grid(double[][] gridArr,double[][] PrevGrid) {
+        int d=0;
+        for(int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                d += Math.abs(gridArr[i][j]-PrevGrid[i][j]);
+            }
+        }
+        if(d< 0.5)
+            return true;
+        else
+            return false;
+    }
+
+
+    public static void copyVal(double[][] gridArr,double[][] prev) {
+       for(int i=0;i<3;i++)
+       {
+           for(int j=0;j<3;j++)
+           {
+               prev[i][j] = gridArr[i][j];
+           }
+       }
+    }
+
+    public static void Value_Iteration(double[][]gridArr,char[][]grid_policy,double[][]PrevGrid,double discount,int[][]rewardArr)
+    {
+        while(! (compare2Grid(gridArr,PrevGrid))){
+
+            copyVal(gridArr,PrevGrid);
+        for (int i=0;i<3 ;i++) {
+            for (int j=0;j<3;j++) {
+                gridArr[i][j] = Math.max(HelpingMethods.Q_value(i, j, grid_policy, gridArr, discount, rewardArr), gridArr[i][j]);
+            }
+        }
+         }
+        HelpingMethods.edit_printPolicy(gridArr,grid_policy);
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int r =sc.nextInt();
+        char[][] grid_policy={{'R','R','R'},{'R','R','R'},{'R','R','R'}};
+        int[][]rewardArr={{r,-1,10},{-1,-1,-1},{-1,-1,-1}};
+        double[][]gridArr={{0,0,0},{0,0,0},{0,0,0}};
+        double[][]PrevGrid ={{1,1,1},{1,1,1},{1,1,1}};
+        double discount=0.99;
+
+        Value_Iteration(gridArr,grid_policy,PrevGrid,0.99,rewardArr);
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.print(gridArr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
